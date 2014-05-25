57894c5ac8091da0b34d776d9b43faf6e3929f93
Olga Demchuk <o.v.demchuk@gmail.com>
2014-05-25 16:04:10 +0400

    На странице мероприятия и в его тумбинашке выводятся изображения шириной 500 px. В блоках с участниками выводится их количество, а количество выводимых ограничено 8 шт. Кнопки редактирования профиля и "выхода" перенесены на show-страницу профиля. Страница редактирования профиля relayouted к примерно тому же виду, что и публичная show-страница Обновлён логотип.

--- app/uploaders/event_title_image_uploader.rb
+++ app/uploaders/event_title_image_uploader.rb
@@ -35,8 +35,8 @@ class EventTitleImageUploader < CarrierWave::Uploader::Base
   #   process :resize_to_fit => [50, 50]
   # end
 
-  version :square_200 do
-    process resize_to_fill: [200, 200]
+  version :square_500 do
+    process resize_to_fill: [500, 500]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
