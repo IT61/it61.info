028b59e2459b9266bfaeb59a989e79e806e807b9
Alex Vasyutin <alex.vasyutin@gmail.com>
2014-06-13 13:34:21 +0400

    Исправлены спеки

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -7,12 +7,26 @@ describe EventsController do
       expect { post :create, event: event_attrs }.to raise_error(CanCan::AccessDenied)
     end
 
-    specify 'member cannot create event' do
+    specify 'member can create event' do
       @user = FactoryGirl.create :user
       login_user
 
       event_attrs = FactoryGirl.attributes_for(:event)
-      expect { post :create, event: event_attrs }.to raise_error(CanCan::AccessDenied)
+      post :create, event: event_attrs
+
+      created_event = Event.first
+      should redirect_to event_path(created_event)
+    end
+
+    specify "event'll be unpublished if member create it" do
+      @user = FactoryGirl.create :admin
+      login_user
+
+      event_attrs = FactoryGirl.attributes_for(:event)
+      post :create, event: event_attrs
+
+      created_event = Event.first
+      expect(created_event).to_not be_published
     end
 
     specify 'admin can create event' do
