047511e5e2380ba5a2cca423faf56cf4ca266b5a
Pavel Plevako <kaminapoke@gmail.com>
2016-02-02 13:57:12 +0300

    PoC of membership requests approve/hide functionality.

--- app/controllers/companies/membership_requests_controller.rb
+++ app/controllers/companies/membership_requests_controller.rb
@@ -1,14 +1,16 @@
 class Companies::MembershipRequestsController < ApplicationController
   respond_to :html
-  load_and_authorize_resource class: Company::MembershipRequest
+  load_and_authorize_resource :company
+  load_and_authorize_resource class: Company::MembershipRequest, :through => :company
   before_filter :set_company, only: :create
+  before_filter :set_user, only: :create
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, flash: { error: exception.message }
   end
 
   has_scope :recent, type: :boolean, allow_blank: true
-  has_scope :by_company, as: :company_id
+  has_scope :hidden, type: :boolean, allow_blank: true, default: false
 
   def index
     @membership_requests = apply_scopes(@membership_requests)
@@ -16,27 +18,38 @@ class Companies::MembershipRequestsController < ApplicationController
   end
 
   def create
-    authorize! :create, @membership_request
-
-    @membership_request.user = current_user
     @membership_request.save!
 
     flash[:success] = t(:company_membership_request_created, title: @membership_request.company.title)
     redirect_to(request.env['HTTP_REFERER'] ? :back : company_path(@membership_request.company))
   end
 
+  def update
+    @membership_request.update_attributes membership_request_params
+    redirect_to :back
+  end
+
+  def hide
+    @membership_request.hide!
+    redirect_to :back
+  end
+
   private
 
   def membership_request_params
-    params.permit(:company_id)
-  end
-
-  def fetch_company
-    @company ||= Company.find(params[:company_id])
+    permitted_attrs = [
+        :company_id,
+        :approved
+    ]
+    params.require(:membership_request).permit(*permitted_attrs)
   end
 
   def set_company
-    @membership_request.company = fetch_company
+    @membership_request.company = @company
+  end
+
+  def set_user
+    @membership_request.user = current_user
   end
 end
 
--- app/models/company/membership_request.rb
+++ app/models/company/membership_request.rb
@@ -4,4 +4,10 @@ class Company::MembershipRequest < ActiveRecord::Base
 
   scope :recent, -> { order(created_at: :desc) }
   scope :by_company, -> (company_id) { where(company_id: company_id) }
+  scope :hidden, ->(hidden) { where(hidden: hidden) }
+
+  def hide!
+    self.hidden = true
+    save!
+  end
 end
