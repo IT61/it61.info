de94ae96b268631845e38d8233f107c7b69c6052
Pavel Plevako <kaminapoke@gmail.com>
2016-02-08 11:21:32 +0300

    Add MembershipRequest model spec.

--- spec/models/membership_request_spec.rb
+++ spec/models/membership_request_spec.rb
@@ -14,4 +14,12 @@ describe Company::MembershipRequest do
       expect { membership_request.approve! }.to change(Company::Member, :count).by(1)
     end
   end
+
+  describe '#hide!' do
+    it 'sets hidden to true and saves it' do
+      membership_request = create(:membership_request)
+
+      expect { membership_request.hide! }.to change { membership_request.reload.hidden }.from(false).to(true)
+    end
+  end
 end
