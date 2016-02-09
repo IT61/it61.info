42f38143061fb2320efe25f8a2cbe9380ecfb6bd
Pavel Plevako <kaminapoke@gmail.com>
2016-02-09 21:00:47 +0300

    Refactor MembershipRequestController spec.

--- spec/controllers/membership_requests_controller_spec.rb
+++ spec/controllers/membership_requests_controller_spec.rb
@@ -1,23 +1,24 @@
 require 'spec_helper'
 
 describe Companies::MembershipRequestsController do
+  let(:user) { create :user }
+  let(:company) { create(:company, founder: user) }
+
+  before do
+    login_user user
+  end
+
   describe 'GET index' do
     before do
-      create_user_and_company
-      get :index, company_id: @company
+      get :index, company_id: company
     end
 
     it { should render_template('index') }
   end
 
   describe 'POST create' do
-    let(:post_request) { post :create, company_id: @company, membership_request: { company_id: @company.id } }
-
-    before do
-      @user = create :user
-      @company = create(:company, :published)
-      login_user
-    end
+    let(:company) { create(:company, :published) }
+    let(:post_request) { post :create, company_id: company, membership_request: { company_id: company.id } }
 
     it 'creates membership request' do
       expect { post_request }.to change(Company::MembershipRequest, :count).by(1)
@@ -27,7 +28,7 @@ describe Companies::MembershipRequestsController do
       post_request
 
       created_request = Company::MembershipRequest.last
-      expect(created_request.user).to eq @user
+      expect(created_request.user).to eq user
     end
 
     it 'sets flash' do
@@ -39,17 +40,13 @@ describe Companies::MembershipRequestsController do
     it 'redirects to company' do
       post_request
 
-      should redirect_to(@company)
+      should redirect_to(company)
     end
   end
 
   describe 'PATCH approve' do
-    let(:membership_request) { create(:membership_request, company: @company) }
-    let(:patch_approve) { patch :approve, company_id: @company, id: membership_request.id }
-
-    before do
-      create_user_and_company
-    end
+    let(:membership_request) { create(:membership_request, company: company) }
+    let(:patch_approve) { patch :approve, company_id: company, id: membership_request.id }
 
     it 'calls #approve! of the membership request' do
       expect_any_instance_of(Company::MembershipRequest).to receive(:approve!)
@@ -65,12 +62,8 @@ describe Companies::MembershipRequestsController do
   end
 
   describe 'PATCH hide' do
-    let(:membership_request) { create(:membership_request, company: @company) }
-    let(:patch_hide) { patch :hide, company_id: @company, id: membership_request.id }
-
-    before do
-      create_user_and_company
-    end
+    let(:membership_request) { create(:membership_request, company: company) }
+    let(:patch_hide) { patch :hide, company_id: company, id: membership_request.id }
 
     it 'calls #hide! of the membership request' do
       expect_any_instance_of(Company::MembershipRequest).to receive(:hide!)
@@ -85,11 +78,11 @@ describe Companies::MembershipRequestsController do
     end
   end
 
-  def create_user_and_company
-    @user = create :user
-    @company = create(:company, founder: @user)
-    login_user
-  end
+  # def create_user_and_company
+  #   @user = create :user
+  #   @company = create(:company, founder: @user)
+  #   login_user
+  # end
 end
 
 
