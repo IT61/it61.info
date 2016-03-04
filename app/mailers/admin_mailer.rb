8b1ce604986ce6a8d6d2a55c32b276426172c17f
Alexey Volobuev <17valexl@gmail.com>
2016-03-04 07:54:36 +0300

    Fix content for admin_mailers

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,35 +1,37 @@
 class AdminMailer < BaseMailer
   
-  def request_to_membership(user, company, member)
-    @user    = user
-    @company = company
-    @member  = member
-
-    subj = I18n.t('admin_mailer.request_to_membership.subject', company: @company.title)
-    mail(to: user.email, subject: subj)
-  end
-
-  def new_company_user(user, new_user)
+  def request_to_membership(user, company, new_user)
     @user     = user
+    @company  = company
     @new_user = new_user
 
-    subj = I18n.t('admin_mailer.new_company_user.subject', user: @new_user.full_name)
+    subj = I18n.t('admin_mailer.request_to_membership.subject', user: @new_user.full_name, company: @company.title)
     mail(to: user.email, subject: subj)
   end
 
-  def delete_user_from_company(user, deleted_user)
+  def new_company_user(user, company, new_user)
+    @user     = user
+    @new_user = new_user
+    @company  = company 
+
+    subj = I18n.t('admin_mailer.new_company_user.subject', user: @new_user.full_name, company: @company.title)
+    mail(to: user.email, subject: subj)
+  end
+
+  def delete_user_from_company(user, company, deleted_user)
     @user         = user
     @deleted_user = deleted_user
+    @company      = company
 
-    subj = I18n.t('admin_mailer.delete_user_from_company.subject', user: @deleted_user.full_name)
+    subj = I18n.t('admin_mailer.delete_user_from_company.subject', user: @deleted_user.full_name, company: @company.title)
     mail(to: user.email, subject: subj)    
   end
 
-  def changing_user_permissions(user, member)
+  def changing_user_permissions(user, copmany, another_user)
     @user   = user
-    @member = member
+    @another_user = another_user
 
-    subj = I18n.t('admin_mailer.changing_user_permissions.subject', user: @member.full_name)
+    subj = I18n.t('admin_mailer.changing_user_permissions.subject', user: @another_user.full_name)
     mail(to: user.email, subject: subj)
   end
 
