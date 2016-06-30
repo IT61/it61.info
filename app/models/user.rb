8f3884dee7ca615fbaefc176d3927e8d5d081af5
Andrew Rudenets <ar@elonsoft.ru>
2016-06-30 16:39:16 +0300

    Merged current User model with model of old project

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and
   devise :database_authenticatable, :registerable,
@@ -10,5 +11,71 @@ class User < ActiveRecord::Base
       user.email = auth.info.email unless auth.info.email.nil?
       user.password = Devise.friendly_token[0,20]
     end
+  end
+
+  has_many :owner_of_events, class_name: 'Event', foreign_key: 'organizer_id'
+  has_many :event_participations, dependent: :destroy
+  has_many :member_in_events, class_name: 'Event', through: :event_participations, source: :event
+
+  validates :email, presence: true, if: 'email_required?'
+  validates :email, uniqueness: true, if: 'email_required?'
+
+  phony_normalize :phone, as: :normalized_phone, default_country_code: 'RU'
+  validates_plausible_phone :phone, country_code: 'RU'
+  validates :phone, presence: true, if: :sms_reminders?
+
+  after_create :assign_default_role
+
+  # В случае, если OAuth провайдер не предоставляет email, в базу может быть записана пустая строка,
+  # что приведет к нарушению уникальности index_users_on_email
+  before_save :nullify_empty_email
+
+  scope :notify_by_email, -> { where(email_reminders: true).where.not(email: nil) }
+  scope :notify_by_sms, -> { where(sms_reminders: true).where.not(phone: nil) }
+  scope :subscribed, -> { where(subscribed: true) }
+  scope :with_email, -> { where.not(email: nil) }
+
+  def login
+    name || email.split('@').first
+  end
+
+  def full_name
+    [first_name, last_name].compact.join(' ').presence || login
+  end
+
+  def event_participations
+    EventParticipation.unscoped { super }
+  end
+
+  def subscribe!
+    self.subscribed = true
+    save!
+  end
+
+  private
+
+  def email_required?
+    # authentications.empty? || name.blank?
+    name.blank?
+  end
+
+  def assign_default_role
+    unless role
+      self.role = :member
+      save!
+    end
+  end
+
+  def restore_event_participations
+    # Идентификаторы заявок пользователя на участие в мероприятиях, которые не удалены
+    ids = EventParticipation.only_deleted.joins(:event)
+              .where('events.deleted_at is null')
+              .where(user_id: id)
+              .pluck(:id)
+    EventParticipation.restore ids
+  end
+
+  def nullify_empty_email
+    self.email = nil unless email.present?
   end
 end
