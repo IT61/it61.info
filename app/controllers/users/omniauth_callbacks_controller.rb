702ab0643a30e5845cd27b37ed71b3fbd6eb85a5
Vladimir Evseev <evseevdev@gmail.com>
2016-07-05 12:15:34 +0300

    Create sign in page (#5)

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   skip_before_filter :verify_authenticity_token
 
+  def signin
+    render "signin";
+  end
+
   def github
     oauth_for 'Github'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {:omniauth_callbacks => "users/omniauth_callbacks"}
   devise_scope :user do
+    get "signin" => "users/omniauth_callbacks#signin"
     delete "/users/sign_out" => "devise/sessions#destroy"
   end
 
