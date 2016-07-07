ecc8a26308d9b150481a4f7be990181e3b871f1c
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-07-07 17:43:47 +0300

    Fix code style

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  root 'application#welcome'
+  root "application#welcome"
 
-  devise_for :users, controllers: {:omniauth_callbacks => "users/omniauth_callbacks"}
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
     get "signin" => "users/omniauth_callbacks#signin"
     delete "/users/sign_out" => "devise/sessions#destroy"
   end
   namespace :users do
-    get 'user_profiles/last_step_register'
+    get "user_profiles/last_step_register"
   end
 
   resources :users, only: [:show]
