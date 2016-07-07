de84ab1725d91e437e3540818602a915bd71bf3a
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-07-07 17:48:30 +0300

    Fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  root "application#welcome"
-
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   devise_scope :user do
     get "signin" => "users/omniauth_callbacks#signin"
