0048e6a1cb39ca130b6af15f4522538fc24d47fc
Vladimir Evseev <evseevdev@gmail.com>
2016-07-11 16:13:27 +0300

    Сделал страницу эвентов, навигацию, поправил типографию заголовков (#16)
    * Rename button class, add extend buttons in header
    
    * Сделал навигацию, сделал страницу мероприятий для десктопов и планшетов
    
    * Подключен jquery mobile для эвентов, сделаны мобильные карточки эвентов, поправлена типография заголовков

---
+++ app/assets/javascripts/mobile.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    // Мобильная навигация
+    if ($(this).width() < 600) {
+        var menuToggle = $('#js-mobile-menu').unbind();
+
+        menuToggle.find("span").text($("li.nav-link.active a").text());
+
+        menuToggle.on('click', function(e) {
+            e.preventDefault();
+            $('#js-navigation-menu').slideToggle(function(){
+                if($('#js-navigation-menu').is(':hidden')) {
+                    $('#js-navigation-menu').removeAttr('style');
+                }
+            });
+        });
+    }
+    
+    // Ограничение по длине названия эвента для мобильных девайсов
+    if ($(this).width() < 500) {
+        $(".event-card h2").each(function () {
+            if ($(this).text().length > 34) {
+                var title = $(this).text().substr(0, 34) + "...";
+                $(this).text(title);
+            }
+        });
+    }
+    
+    // Свайпы в мобильной версии карточек эвентов
+    $(".event-card").on("swipeleft", function () {
+        $(this).addClass("event-swipe");
+    }).on("swiperight", function () {
+        $(this).removeClass("event-swipe");
+    });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-# frozen_string_literal: true
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  # root 'application#welcome'
+
+  devise_for :users, controllers: {:omniauth_callbacks => "users/omniauth_callbacks"}
   devise_scope :user do
     get "signin" => "users/omniauth_callbacks#signin"
     delete "/users/sign_out" => "devise/sessions#destroy"
