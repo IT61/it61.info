32bbf50709326552d27a736466dffb1ff19bfa71
Andrew Rudenets <ar@elonsoft.ru>
2016-07-13 18:12:34 +0300

    Add autoresize for textareas

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -4,4 +4,15 @@ $(document).ready(function () {
     $(".input-float-label input, textarea").blur(function () {
         $(this).toggleClass('top-label', this.value !== "");
     }).trigger('blur');
+
+    // Autoresize
+    function h(element) {
+        $(element).css({'height': 'auto', 'overflow-y': 'hidden'}).height(element.scrollHeight);
+    }
+
+    $('textarea').each(function () {
+        h(this);
+    }).on('input', function () {
+        h(this);
+    });
 });
