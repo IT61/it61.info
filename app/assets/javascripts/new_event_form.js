1398b06b6061f45f7f249772480b23725c7b1edb
nhpd <dmitry.klpv@gmail.com>
2016-07-13 17:08:31 +0300

    add image to edit page

--- app/assets/javascripts/new_event_form.js
+++ app/assets/javascripts/new_event_form.js
@@ -1,61 +1,63 @@
 function handleFileUpload() {
-  var wrapper = $( ".file_upload" ),
-      inp = wrapper.find( "input" ),
-      btn = wrapper.find( "button" ),
-      lbl = wrapper.find( "label" );
+    var wrapper = $(".file_upload"),
+        inp = wrapper.find("input"),
+        btn = wrapper.find("button"),
+        lbl = wrapper.find("label");
 
-  btn.focus(function(){
-      inp.focus()
-  });
-  // Crutches for the :focus style:
-  inp.focus(function(){
-      wrapper.addClass( "focus" );
-  }).blur(function(){
-      wrapper.removeClass( "focus" );
-  });
+    btn.focus(function () {
+        inp.focus()
+    });
+    // Crutches for the :focus style:
+    inp.focus(function () {
+        wrapper.addClass("focus");
+    }).blur(function () {
+        wrapper.removeClass("focus");
+    });
 
-  var file_api = ( window.File && window.FileReader && window.FileList && window.Blob ) ? true : false;
+    var file_api = ( window.File && window.FileReader && window.FileList && window.Blob ) ? true : false;
 
-  inp.change(function(){
-      var file_name;
-      if( file_api && inp[ 0 ].files[ 0 ] )
-          file_name = inp[ 0 ].files[ 0 ].name;
-      else
-          file_name = inp.val().replace( "C:\\fakepath\\", '' );
+    inp.change(function () {
+        var file_name;
+        if (file_api && inp[0].files[0])
+            file_name = inp[0].files[0].name;
+        else
+            file_name = inp.val().replace("C:\\fakepath\\", '');
 
-      if( ! file_name.length )
-          return;
+        if (!file_name.length)
+            return;
 
-      if( lbl.is( ":visible" ) ){
-          lbl.text( file_name );
-          btn.text( "Выбрать файл" );
-      } else
-          btn.text( file_name );
-  }).change();
+        if (lbl.is(":visible")) {
+            lbl.text(file_name);
+            btn.text("Выбрать файл");
+        } else
+            btn.text(file_name);
+    }).change();
 
-  $( window ).resize(function(){
-    $( ".file_upload input" ).triggerHandler( "change" );
-  });
+    $(window).resize(function () {
+        $(".file_upload input").triggerHandler("change");
+    });
 }
 
 function fileReaderSupported() {
-  return window.File && window.FileReader && window.FileList && window.Blob
+    return window.File && window.FileReader && window.FileList && window.Blob
 }
 
 function showImageOnLoad() {
-  if (!document.getElementById('event_title_image')) {
-    return;
-  }
-  
-  document.getElementById('event_title_image').onchange = function () {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      document.getElementById('image').src = e.target.result;
-    };
-    if (this.files.length !== 0) {
-      reader.readAsDataURL(this.files[0]);
+    var $imageInput = document.getElementById('image_input'),
+        $image = document.getElementById('image');
+    if (!$imageInput) {
+        return;
     }
-  };
+
+    $imageInput.onchange = function () {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $image.src = e.target.result;
+        };
+        if (this.files.length !== 0) {
+            reader.readAsDataURL(this.files[0]);
+        }
+    };
 }
 
 $(document).ready(function () {
