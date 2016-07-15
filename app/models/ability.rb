b20de8e41223b5f5b9b0f272f77231fb1a3ba2f5
nhpd <dmitry.klpv@gmail.com>
2016-07-15 13:10:59 +0300

    refine user abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,20 +2,38 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    return if user.nil?
+    return if user.nil? || user.role.nil?
 
     alias_action :create, :read, :update, :destroy, :to => :crud
+
+    # todo: add rules for companies later
 
     if user.admin?
       can :manage, :all
     elsif user.moderator?
-      can [:read, :edit, :publish!], Event, published: false
+      can [:read, :edit, :publish!], Event, not_published?
       can :index, User
       # todo: can view and edit event participants
     end
 
-    can [:read, :participate], Event, published: true
+    can [:read, :participate], Event,
+    can [:create, :read, :update], Event, organizer?
+
     can :show, User
     can :crud, User, id: user.id
+  end
+
+  private
+
+  def organizer?(user)
+    { organizer_id: user.id }
+  end
+
+  def published?
+    { published: true }
+  end
+
+  def not_published?
+    { published: false }
   end
 end
