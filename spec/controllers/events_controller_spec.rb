3530fabdb6f146fcc9e4109e014162b78a05ea4b
Dmitry Kolupaev <dmitry.klpv@gmail.com>
2016-07-19 15:53:10 +0300

    fix tests (#29)

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -23,9 +23,9 @@ RSpec.describe EventsController, type: :controller do
   end
 
   describe 'GET #index' do
-    it 'returns http success' do
+    it 'returns http redirect' do
       get :index
-      expect(response.status).to eq(200)
+      expect(response.status).to eq(302)
     end
   end
 
