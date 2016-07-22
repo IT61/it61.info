52fa990bb46641962b9828fe58beafb62ae0c07c
Andrew Rudenets <ar@elonsoft.ru>
2016-07-22 12:50:06 +0300

    Implemented Instagram caching

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720130625) do
+ActiveRecord::Schema.define(version: 20160722085828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
