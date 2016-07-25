a7d8d8fb89bb45feb17eef0dd9bfe510b4d777cd
nhpd <dmitry.klpv@gmail.com>
2016-07-25 10:31:01 +0300

    add places to admin_panel

--- app/controllers/admin/places_controller.rb
+++ app/controllers/admin/places_controller.rb
@@ -3,5 +3,32 @@ class Admin::PlacesController < ApplicationController
   before_action :authenticate_admin!
 
   def index
+    @places = Place.paginate page: params[:page], per_page: 10
   end
+
+  def edit
+    @place = fetch_place
+  end
+
+  def update
+    @place = fetch_place
+    commit = @place.update place_params
+    if commit
+      redirect_to '/admin/places', notice: 'Данные места обновлены'
+    else
+      flash.now[:error] = 'Не получилось обновить место'
+      render 'edit'
+    end
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit :address, :title, :latitude, :longitude
+  end
+
+  def fetch_place
+    Place.find params[:id]
+  end
+
 end
