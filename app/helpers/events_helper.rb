cb71e74ba6ddadb0e6adebdd062b2bb31abc1896
Andrew Rudenets <ar@elonsoft.ru>
2016-07-28 15:31:25 +0300

    Events: Locales for index/new/show

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -13,16 +13,16 @@ module EventsHelper
   def participate_in_event_link(event)
     if Event.upcoming.exists?(event)
       if event.opened?
-        link_to "Я пойду", participate_event_path(event), method: :post, class: "btn btn-shadow btn-blue"
+        link_to t("events.participations.participate"), participate_event_path(event), method: :post, class: "btn btn-shadow btn-blue"
       else
-        link_to "Зарегистрироваться", register_to_event_path(event), class: "btn btn-shadow btn-blue"
+        link_to t("events.participations.register"), register_to_event_path(event), class: "btn btn-shadow btn-blue"
       end
     else
-      link_to "Я участвовал", participate_event_path(event), method: :post, class: "btn btn-shadow btn-blue"
+      link_to t("events.participations.participated"), participate_event_path(event), method: :post, class: "btn btn-shadow btn-blue"
     end
   end
 
   def revoke_registration_link(event)
-    link_to "Отменить заявку", revoke_participation_event_path(event), method: :delete, class: "btn btn-shadow btn-blue"
+    link_to t("events.participations.revoke_participation"), revoke_participation_event_path(event), method: :delete, class: "btn btn-shadow btn-blue"
   end
 end
