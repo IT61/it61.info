7a9c829c0daaff6558cbedc70097387e2c9b9cfd
Andrew Rudenets <ar@elonsoft.ru>
2016-08-01 14:29:50 +0300

    Use gem 'config' for Settings

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   def index
-    @tag = APP_CONFIG.community.instagram_hashtag
+    @tag = Settings.community.instagram_hashtag
     @photos = InstagramService.photos(@tag).take(15)
   end
 end
--- config/initializers/appconfig.rb
+++
@@ -1,7 +0,0 @@
-require "ostruct"
-require "yaml"
-require "erb"
-
-all_config = YAML.load_file(Rails.root.join("config", "config.yml"))
-APP_CONFIG = JSON.parse(all_config.to_json, object_class: OpenStruct)
-ERB::APP_CONFIG = APP_CONFIG
