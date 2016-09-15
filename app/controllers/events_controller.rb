8615bf8ee9398cb736f93fab60584711954e2611
Andrew Rudenets <ar@elonsoft.ru>
2016-09-15 15:33:34 +0300

    Page limits moved to settings.yml

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -100,7 +100,7 @@ class EventsController < ApplicationController
   end
 
   def show_events(scope, all = false)
-    @events = Event.send(scope).paginate(page: params[:page], per_page: 6).eager_load(:place)
+    @events = Event.send(scope).published.paginate(page: params[:page], per_page: Settings.per_page.events).eager_load(:place)
     @events = @events.published unless all
     @scope = scope
 
