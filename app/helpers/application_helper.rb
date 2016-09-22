217fdb0466d975a5f6fac1a9f4cd309a5cca1226
Andrew Rudenets <ar@elonsoft.ru>
2016-09-22 12:08:36 +0300

    Interaction with Redcarpet gem moved to service

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,12 @@
 module ApplicationHelper
-  # rubocop:disable Rails/OutputSafety
+
   def markdown(text)
-    md ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(escape_html: true, hard_wrap: true),
-                                   autolink: true,
-                                   space_after_headers: false,
-                                   lax_spacing: true,
-                                   tables: true)
-    raw(md.render(text))
+    # rubocop:disable Rails/OutputSafety
+    raw(MarkdownService.render_markdown(text))
   end
 
   def plain_text(text)
-    plain ||= Redcarpet::Markdown.new(Redcarpet::Render::StripDown, space_after_headers: false)
-    plain.render(text)
+    MarkdownService.render_plain(text)
   end
 
   def render_editor?
