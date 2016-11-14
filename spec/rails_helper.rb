01e7bf25a559e27798ea51a222a2c3ae8e2de070
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-11-15 01:14:40 +0300

    Setup local code coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,21 @@
 if ENV.fetch("COVERAGE", false)
   require "simplecov"
 
+  SimpleCov.profiles.define "coverage" do
+    load_profile "rails"
+    # Don't include vendored stuff
+    add_filter ".vendor"
+    add_filter "lib/tasks"
+
+    add_group "Uploaders", "app/uploaders"
+  end
+
   if ENV["CIRCLE_ARTIFACTS"]
     dir = File.join(ENV["CIRCLE_ARTIFACTS"], "coverage")
     SimpleCov.coverage_dir(dir)
   end
 
-  SimpleCov.start "rails"
+  SimpleCov.start "coverage"
 end
 
 ENV["RAILS_ENV"] ||= "test"
