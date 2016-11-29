217dc1fc879edc2e498e162354e33a54422211d6
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-11-29 14:34:45 +0300

    Remove Company abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,18 +14,9 @@ class Ability
       can :manage, User
       can :publish, Event
       can :manage, EventParticipation
-
-      can :manage, Company
-      can :publish, Company
-      can :manage, CompanyMember
     end
 
     if user.member?
-      can :create, Company
-      can :update, Company, founder_id: user.id
-      can :view, Company, founder_id: user.id
-      cannot :publish, Company
-
       can :create, Event
       can :view, Event, organizer_id: user.id
       can :update, Event, organizer_id: user.id
@@ -33,8 +24,6 @@ class Ability
 
       can :create, EventParticipation
       can :destroy, EventParticipation, user_id: user.id
-      can :create, CompanyMember
-      can :destroy, CompanyMember, user_id: user.id
     end
   end
 end
