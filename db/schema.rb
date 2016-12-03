1f9259939c022592952c69a375a5e2a83b8da9b7
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-12-03 13:11:38 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -59,11 +59,12 @@ ActiveRecord::Schema.define(version: 20161114214504) do
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name", using: :btree
   end
 
   create_table "groups_users", id: false, force: :cascade do |t|
-    t.integer "group_id"
-    t.integer "user_id"
+    t.integer "group_id", null: false
+    t.integer "user_id",  null: false
     t.index ["group_id"], name: "index_groups_users_on_group_id", using: :btree
     t.index ["user_id"], name: "index_groups_users_on_user_id", using: :btree
   end
