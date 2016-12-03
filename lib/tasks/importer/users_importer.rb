ec4acb33f2c8342cbc430a3f298345dd2e833ec0
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-12-03 15:11:18 +0300

    Do not import user avatar

--- lib/tasks/importer/users_importer.rb
+++ lib/tasks/importer/users_importer.rb
@@ -27,8 +27,6 @@ module UsersImporter
         user.write_attribute(field_name, row.get(field_name))
       end
 
-      user.write_attribute("avatar", row.get("avatar_image"))
-
       # create migration token
       if has_password
         user.write_attribute("migration_token", SecureRandom.hex)
