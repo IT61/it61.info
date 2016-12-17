620776409d6016e943744b10357bda56ae0bd577
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2016-12-17 10:50:24 +0300

    Add more translations for User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216082402) do
+ActiveRecord::Schema.define(version: 20161216191002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,9 +56,10 @@ ActiveRecord::Schema.define(version: 20161216082402) do
   end
 
   create_table "groups", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",                   null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "kind",       default: 0, null: false
     t.index ["name"], name: "index_groups_on_name", using: :btree
   end
 
