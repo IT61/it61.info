0b6c613482ffecb6e03cec73d9addbb1bbd8f8da
Andrew Rudenets <andrey.rudenets@gmail.com>
2016-12-31 00:27:53 +0300

    Add meta tags for site pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,39 @@
 module ApplicationHelper
+
+  def default_meta_image
+    {
+      _: asset_url("share.png"),
+      width: 1200,
+      height: 630
+    }
+  end
+
+  def default_meta_tags
+    {
+      site: Settings.community.title,
+      reverse: true,
+      separator: raw("&ndash;"),
+      description: Settings.community.description,
+      image_src: asset_url("share.png"),
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        url: root_url,
+        locale: "ru_RU",
+        type: "website",
+        image: default_meta_image
+      },
+      twitter: {
+        image: asset_url("share.png"),
+        card: "summary_large_image",
+        site: "it_rostov",
+        title: :title,
+        description: :description
+      }
+    }
+  end
+
   def markdown(text)
     raw(MarkdownService.render_markdown(text))
   end
@@ -8,6 +43,6 @@ module ApplicationHelper
   end
 
   def social_networks
-    Dir["#{Rails.root}/app/views/application/widgets/*"].sort_by {|file| file.scan(/\d+/).last.to_i }
+    Dir["#{Rails.root}/app/views/application/widgets/*"].sort_by { |file| file.scan(/\d+/).last.to_i }
   end
 end
