e857cce85dc279e6f621635362a3d19525fd3087
Yuri Tkachenko <yuri.tam.tkachenko@gmail.com>
2017-01-18 21:02:07 +0700

    some functions for dev

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,6 @@ ActiveRecord::Schema.define(version: 20161221144006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "companies", force: :cascade do |t|
-    t.string   "name",        null: false
-    t.text     "description"
-    t.string   "url"
-    t.string   "logo"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["name"], name: "index_companies_on_name", unique: true, using: :btree
-  end
-
   create_table "event_participations", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "event_id"
