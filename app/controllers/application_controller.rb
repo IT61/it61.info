c3fed670621b8ae4877d4580e7193d9435c7c1c7
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2017-02-05 19:02:33 +0300

    Always print info from cancancan to log

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
+    Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
+
+    throw exception if Rails.env.development?
     if user_signed_in? && current_user.fresh?
       redirect_to sign_up_complete_path
     else
-      Rails.logger.debug "Access denied on #{exception.action} #{exception.subject.inspect}"
-      throw exception unless Rails.env.production?
       redirect_to new_session_path
     end
   end
