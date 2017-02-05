d462001a3f0a4e381db0290c4e19f2a307e858ea
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2017-02-05 19:00:25 +0300

    Remove bundled serializeObject.
    This gem (library) doesn't work properly with nested objects.

--- app/assets/javascripts/events/event_manager.js
+++ app/assets/javascripts/events/event_manager.js
@@ -1,3 +1,19 @@
+$.fn.serializeObject = function () {
+  var o = {};
+  var a = this.serializeArray();
+  $.each(a, function () {
+    if (o[this.name] !== undefined) {
+      if (!o[this.name].push) {
+        o[this.name] = [o[this.name]];
+      }
+      o[this.name].push(this.value || '');
+    } else {
+      o[this.name] = this.value || '';
+    }
+  });
+  return o;
+};
+
 var eventManager = {
   initEditor: function () {
     var editorElem = document.getElementById('editor');
