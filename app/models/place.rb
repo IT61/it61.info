8274e7924ac53d093d81b4966cd3f96d5f1bf0c1
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2017-02-05 19:02:07 +0300

    Validate Place's title

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,7 @@ class Place < ApplicationRecord
   has_many :events
 
   validates :address, presence: true
+  validates :title, presence: true
 
   def full_address
     [address, title].compact.reject(&:empty?).join(", ")
