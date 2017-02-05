1f2f0cb7d56cfcc3fa1dbfe8436b8dcd161f0eb1
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2017-02-05 18:58:41 +0300

    Add mini_magick

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,7 @@
-# encoding: utf-8
-# frozen_string_literal: true
-
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
