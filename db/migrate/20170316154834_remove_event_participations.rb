c7a8e9579362fb2895410683016426af919484ec
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2017-03-30 14:03:56 +0300

    fixup! Rename *Participations to *Attendees

--- db/migrate/20170316154834_remove_event_participations.rb
+++ db/migrate/20170316154834_remove_event_participations.rb
@@ -1,8 +1,8 @@
 class RemoveEventParticipations < ActiveRecord::Migration[5.0]
   def change
     execute %{
-INSERT INTO events_attendees(user_id, event_id)
-SELECT user_id, event_id FROM event_participations;
+INSERT INTO events_attendees(user_id, event_id, created_at, updated_at)
+SELECT user_id, event_id, current_timestamp, current_timestamp FROM event_participations;
 }
     drop_table :event_participations
     drop_table :registrations
