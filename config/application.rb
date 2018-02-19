dd7d577dc2b34d95cb2149b8d96e5ed2cd765b48
Vitaly Slobodin <vitaliy.slobodin@gmail.com>
2018-02-19 18:09:12 +0300

    Move I18n to its own initializator

--- config/application.rb
+++ config/application.rb
@@ -10,28 +10,17 @@ require "action_view/railtie"
 require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
+
 module It61
   class Application < Rails::Application
     config.app_generators.scaffold_controller :responders_controller
 
-    config.generators do |generate|
-      generate.helper false
-      generate.javascript_engine false
-      generate.request_specs false
-      generate.routing_specs false
-      generate.stylesheets false
-      generate.test_framework :rspec
-      generate.view_specs false
-    end
     config.eager_load_paths += ["#{Rails.root}/lib"]
+
     config.exceptions_app = self.routes
 
     config.action_controller.action_on_unpermitted_parameters = :raise
     config.action_view.raise_on_missing_translations = true
-
-    config.i18n.load_path += Dir["#{Rails.root}/config/locales/**/*.{rb,yml}"]
-    config.i18n.available_locales = [:ru, :en]
-    config.i18n.default_locale = :ru
 
     config.assets.paths << Rails.root.join("vendor", "assets", "components")
 
